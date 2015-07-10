648f2972e7934008d2cb07f2b0f64769a4e4c534
k1211059 <k1211059@edu.cc.uec.ac.jp>
2015-07-10 15:24:30 +0900

    player changed 0710-2

--- player.rb
+++ player.rb
@@ -11,20 +11,16 @@ class Player
       @myHand[i] = -1
       @opponentHand[i] = -1
     end
-    @myChip = 0
     @myTotal = -1
-    @boardBet = 0
-    @myAct.action = 0
-    @myAct.betChip = 0
   end
   
   
   def actionCard
     puts "action? (1:Hit 0:Stand)"
     while 1 do
-      myAct = STDIN.gets
-      if myAct.to_i==1 || myAct.to_i==0 then
-        return myAct.to_i
+      myAct = STDIN.gets.to_i
+      if myAct==1 || myAct==0 then
+        return myAct
       else
         puts "please retype (1or0)"
       end
@@ -32,20 +28,20 @@ class Player
   end
 
   def actionBet
-    @myChip = gunyagunya.chip
+    myChip = gunyagunya.chip
     
-    if boardBet.to_i==0 then
+    if boardBet==0 then
       puts "action? (check[1] or bet[3])"
       while 1 do
-        myBet = STDIN.gets
-        if myBet.to_i==1 then
-          return myBet.to_i
-        elsif myBet.to_i==3 then
-          if @myChip < 20 then
+        myBet.action = STDIN.gets.to_i
+        if myBet.action==1 then
+          return myBet.action
+        elsif myBet.action==3 then
+          if myChip < 20 then
             puts "All in"
-            return myBet.to_i
+            return myBet.action
           else
-            return myBet.to_i
+            return myBet.action
           end
         else
           puts "please retype (1or3)"
@@ -54,9 +50,9 @@ class Player
     else
       puts "action? (fold[0] , call[2] or raise[3])"
       while 1 do
-        myBet = STDIN.gets
-        if myBet.to_i==0 || myBet.to_i==2 || myBet.to_i==3 then
-          return myBet.to_i
+        myBet.action = STDIN.gets.to_i
+        if myBet.action==0 || myBet.action==2 || myBet.action==3 then
+          return myBet.action
         else
           puts "please retype (0,2or3)"
         end
